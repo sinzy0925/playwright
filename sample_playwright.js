dcb2e377b3805247120cbac4a779a92ad2939aee
sinzy0925 <84183154+sinzy0925@users.noreply.github.com>
2023-11-16 10:54:50 +0900

    Add files via upload

--- sample_playwright.js
+++ sample_playwright.js
@@ -8,38 +8,71 @@ const { chromium } = require('playwright');//Chromiumというブラウザを使
     //headless: true  //false 画面あり　true 画面なし
     headless: false //false 画面あり　true 画面なし
   });
-  const context = await browser.newContext();
-  const page = await context.newPage();
-  const timeout  = 30000;
-  page.setDefaultTimeout(timeout);
+  const context = await browser.newContext();//contextを使う宣言をする
+  const page = await context.newPage();            //pageを使う宣言をする
+  const timeout  = 30000;                    //timeoutを30000ミリ秒に設定する
+  page.setDefaultTimeout(timeout);   //デフォルトタイムアウトを30000ミリ秒に設定する。
+  await page.setViewportSize({
+    width:  750,
+    height: 550,
+  });//ブラウザの大きさを設定する。
 
-  //ここまでは、とりあえず書いておくので、説明しません。
+//ここまでは、とりあえず書いておくので、説明しません。
 
-  //プログラムを書いていく場所　スタート
+//ブロックでコメントする場合/*　*/で囲う
+/*
+いろいろ書いてますが、ブラウザにさせることは、以下の５つです。
+①https://www.google.co.jp/へ行く
+②検索窓に、googleと入れる
+③Enterキーを押す
+④スクリーンショットを撮る
+⑤ブラウザを閉じる
+*/
 
-  console.log('goto url gooleのサイトへ行く'); //状況を把握するためにログを出力してます。
+//プログラムを書いていく場所　スタート
+
+  //timeout1を3000ミリ秒(3秒)に設定する。
+  //この後この変数を使いまわす。
+  let timeout1 = 3000;
+
+  //状況を把握するために黒い画面にログを出力します。　
+  //ログ内容：goto url gooleのサイトへ行く
+  console.log('①https://www.google.co.jp/へ行く'); 
+  await page.waitForTimeout(timeout1);//timeout1の時間待つ
   await page.goto('https://www.google.co.jp/');//Googleのサイトへ行きます
+  //①https://www.google.co.jp/へ行く
 
-  await page.waitForTimeout(1000);
-  console.log('検索ボックスへgoogleと入力'); 
+  console.log('②検索窓に、googleと入れる');//ログ内容：検索ボックスへgoogleと入力
+  await page.waitForTimeout(timeout1);//timeout1の時間待つ
   await page.getByRole('combobox', { name: '検索' }).fill('google');
+  //②検索窓に、googleと入れる
 
-  await page.waitForTimeout(1000);
-  console.log('エンターキーを押す'); 
+  console.log('③Enterキーを押す');//ログ内容：エンターキーを押す
+  await page.waitForTimeout(timeout1);//timeout1の時間待つ
   await page.getByRole('combobox', { name: '検索' }).press('Enter');
+  //③Enterキーを押す
 
-  await page.waitForTimeout(1000);
-  console.log('スクリーンショットを撮る') 
+  console.log('④スクリーンショットを撮る'); //ログ内容：スクリーンショットを撮る
+  await page.waitForTimeout(timeout1);//timeout1の時間待つ
   await page.screenshot({ path: 'screenshot.png', fullPage: true });
+  //④スクリーンショットを撮る。フルページで。
 
-  console.log('browser.close()');
+  console.log('⑤ブラウザを閉じる。プログラム終わり');//ログ内容：ブラウザを閉じる
+  await page.waitForTimeout(timeout1);//timeout1の時間待つ
 
-  //プログラムを書いていく場所　エンド
+//プログラムを書いていく場所　エンド
+
+//ブロックでコメントする場合/*　*/で囲う
+/*
+  console.log('プログラム終了');//ログ内容：プログラム終了
+  console.log('プログラム終了');//ログ内容：プログラム終了
+*/
 
   //作ったのと逆順で閉じる
   await page.close();
   await context.close();
   await browser.close();
+  //⑤ブラウザを閉じる
 
 })();
 
